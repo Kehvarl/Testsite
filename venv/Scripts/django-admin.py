2048e93a9f54280707d5c27c7c14a892eaac1f8f
Adam Corbett <kehvarl@gmail.com>
2020-02-18 20:29:16 -0700

    create django app

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Adam\Documents\Development\Python\Testsite\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
