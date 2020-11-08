00bef56496137ef38962a728fa530c8e33e8c518
kehvarl <kehvarl@kehvarl.com>
2020-11-08 11:42:39 -0700

    Debug so page and view actually display.

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
-class HomePage(ListView):
+class HomePageView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'posts/home.html'
     context_object_name = 'all_posts_list'
