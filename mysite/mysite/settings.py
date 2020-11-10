dfe284f8ee202c2f26f34ceb540e51d045590077
kehvarl <kehvarl@kehvarl.com>
2020-11-09 18:01:59 -0700

    Create blog model and admin entries for model

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
     'posts.apps.PostsConfig',
-    'blog.app.BlogConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
